1466ba3cbfa9bdaacdf5bc462db48c6d5a19b99a
Bahaagac <48641679+Bahaagac@users.noreply.github.com>
2020-10-16 01:27:32 +0300

    State

--- reactapp/src/components/User.js
+++ reactapp/src/components/User.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
  class User extends Component {
+     
+    state = {
+        isVisible: false
+    }
+
+    // constructor(props){
+    //     super(props);
+
+    //     this.state = {
+    //         isVisible: false
+    //     }
+    // }
+
     render() {
 
         // Destructing
         const {name,department,salary} = this.props
+        const {isVisible} = this.state;
 
         return (
             <div className="col-md-8 mb-4">
@@ -14,10 +28,14 @@ import PropTypes from 'prop-types'
                         <h3 className="d-inline">{name}</h3>
                         <i className="far fa-trash-alt" style= {{cursor: "pointer"}}></i>
                     </div>
-                    <div className="card-body">
-                        <p className="card-text">Department : {department}</p>   
-                        <p className="card-text">Salary : {salary}</p>    
-                    </div>
+                    {
+                        isVisible ?
+                        <div className="card-body">
+                            <p className="card-text">Department : {department}</p>   
+                            <p className="card-text">Salary : {salary}</p>    
+                        </div> : null
+                    }
+                    
                 </div>                    
             </div>
         )
